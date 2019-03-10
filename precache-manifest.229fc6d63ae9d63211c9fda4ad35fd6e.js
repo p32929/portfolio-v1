b5fe38331122fe0daeb7302a79175ad351122dd7
p32929 <p32929@yahoo.com>
2019-03-10 15:23:15 +0600

    Updates

---
+++ precache-manifest.229fc6d63ae9d63211c9fda4ad35fd6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.8760695a.js"
+  },
+  {
+    "revision": "a5cf4d1048f5154924cd",
+    "url": "/Portfolio/static/js/main.5fb9deaf.chunk.js"
+  },
+  {
+    "revision": "e8f385740d088e23d2f5",
+    "url": "/Portfolio/static/js/2.bf937511.chunk.js"
+  },
+  {
+    "revision": "a5cf4d1048f5154924cd",
+    "url": "/Portfolio/static/css/main.f2734c55.chunk.css"
+  },
+  {
+    "revision": "6000b0d47cc96d229638a649e0190b53",
+    "url": "/Portfolio/index.html"
+  }
+];
