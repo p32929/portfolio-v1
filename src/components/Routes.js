490543d249e6a72c376883ed842ba8325d7213df
p32929 <p32929@yahoo.com>
2019-03-10 15:41:05 +0600

    not found

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,13 +4,15 @@ import Home from "./Home";
 import Skills from "./Skills";
 import Works from "./Projects";
 import Contact from "./Contact";
+import NotFound from "./NotFound";
 
 const Routes = (props) => (
     <Switch>
-        <Route path='/' component={Home} exact={true} />
+        <Route path='/' component={Home} exact={true}/>
         <Route path='/skills' component={Skills}/>
         <Route path='/projects' component={Works}/>
         <Route path='/contact' component={Contact}/>
+        <Route component={NotFound}/>
     </Switch>
 )
 
