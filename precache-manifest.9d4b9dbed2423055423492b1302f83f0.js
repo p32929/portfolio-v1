fe45c7ae272f0da15457b494dbb3f8ca0f22b187
p32929 <p32929@yahoo.com>
2019-03-21 10:36:51 +0600

    Updates

---
+++ precache-manifest.9d4b9dbed2423055423492b1302f83f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.8760695a.js"
+  },
+  {
+    "revision": "55bccbc2d8ad6a276ea9",
+    "url": "/Portfolio/static/js/main.dc404d4e.chunk.js"
+  },
+  {
+    "revision": "76f68fef55201af9fb3f",
+    "url": "/Portfolio/static/js/2.cf3d37d8.chunk.js"
+  },
+  {
+    "revision": "55bccbc2d8ad6a276ea9",
+    "url": "/Portfolio/static/css/main.f2734c55.chunk.css"
+  },
+  {
+    "revision": "d22eac56af615a360eba3b901f653f14",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.d44ea572b026e6f72e3c7a8a446618bc.js"
+  "/Portfolio/precache-manifest.9d4b9dbed2423055423492b1302f83f0.js"
 );
 
 workbox.clientsClaim();
