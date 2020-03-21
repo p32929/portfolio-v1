52ec5152bf194c28f5f28b8c59ea1a5383ac07e3
p32929 <p32929@yahoo.com>
2020-03-21 12:59:48 +0600

    fixed missing links image

--- src/vars/skillItems.js
+++ src/vars/skillItems.js
@@ -112,7 +112,7 @@ export var frameworks = [
     },
     {
         title: 'Bootstrap',
-        logo: 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Boostrap_logo.svg'
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png'
     },
     {
         title: 'MongoDB',
@@ -195,8 +195,8 @@ export var others = [
         logo: 'https://www.vectorlogo.zone/logos/arduino/arduino-icon.svg'
     },
     {
-        title: 'IntelliJ IDA',
-        logo: 'https://www.jetbrains.com/company/brand/img/jb-logo-with-grid.svg'
+        title: 'IntelliJ IDEA',
+        logo: 'https://resources.jetbrains.com/storage/products/intellij-idea/img/meta/intellij-idea_logo_300x300.png'
     },
     {
         title: 'Webstorm',
